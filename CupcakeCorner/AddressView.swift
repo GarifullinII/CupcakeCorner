d9fbee2b7c5860bc9e40625966d102ccdd8da1c6
GarifullinII <ildaridrisovich@gmail.com>
2024-07-18 12:43:58 +0400

    add Order, AddressView

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Ildar Garifullin on 18/07/2024.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView()
+}
---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Ildar Garifullin on 18/07/2024.
+//
+
+import Foundation
